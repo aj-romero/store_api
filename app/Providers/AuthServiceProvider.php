d029552ea9b3e1fda4b04a13775f85aafec6be86
Alber Romero <alberjose.romero@gmail.com>
2019-02-02 17:01:34 -0600

    setting up user conf and role conf, adding all necesary

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use Laravel\Passport\Passport;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
