930a8f4e4e252b275fbfc62068c267b28453d4db
Alber Romero <alberjose.romero@gmail.com>
2019-02-02 17:25:30 -0600

    set up react with laravel

---
+++ resources/js/components/Main.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+ 
+/* An example React component */
+class Main extends Component {
+	constructor(){
+		super();
+		this.state = { products: [],}	
+	}
+	
+	componentDidMount() {
+		    /* fetch API in action */
+		    fetch('/api/products')
+			.then(response => {
+			    return response.json();
+			})
+			.then(products => {
+			    //Fetched product is stored in the state
+							    
+				this.setState({products:products.data});
+			});
+		  }
+	renderProducts(){
+		return this.state.products.map(product =>{
+			return(	<tbody>
+						<tr key={product.id}>
+							<td>{product.id}</td>
+							<td>{product.name}</td>
+							<td>{product.price}</td>
+							<td>{product.quantity}</td>
+							<td>{product.likes}</td>
+						</tr>
+					</tbody>
+							
+			);		
+		});	
+	}
+    render() {
+
+	
+        return (
+            <div>
+                <h3>All Products In DB</h3>
+		
+			<div className="col-md-6 mt-4 text-center">
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th>Id Product</th>
+							<th>name</th>
+							<th>Price</th>
+							<th>Quantity</th>
+							<th>Likes</th>
+						</tr>					
+					</thead>
+					{ this.renderProducts() }
+				</table>
+			</div>
+            </div>
+        );
+    }
+}
+ 
+export default Main;
+ 
+/* The if statement is required so as to Render the component on pages that have a div with an ID of "root";  
+*/
+ 
+if (document.getElementById('root')) {
+    ReactDOM.render(<Main />, document.getElementById('root'));
+}
