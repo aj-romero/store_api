ceb0d2e7c7ebcdc07d49371eb0b358d2f031513c
Alber Romero <alberjose.romero@gmail.com>
2019-02-02 16:47:52 -0600

    setting up passport with routes

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'AuthController@login');
+Route::post('signup', 'AuthController@signup');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::group(['middleware' => 'auth:api'], function() {
+                Route::get('logout', 'AuthController@logout');			
+                Route::get('user', 'AuthController@user');
+                Route::get('testmessage', 'AuthController@message');
+                Route::post('purchaseorders','PurchaseOrdersController@store');
+  });
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
