c4b0d04b8a2b5363ddfdb0eab778ce9fc4ab0808
Alber Romero <alberjose.romero@gmail.com>
2019-02-02 16:32:53 -0600

    installing passport and setting up User Model Entity

--- app/User.php
+++ app/User.php
@@ -6,9 +6,12 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+//Laravel - Passport
+Use Laravel\Passport\HasApiTokens;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -25,6 +28,50 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
-    ];
+        		'password', 'remember_token',
+				];
+
+    	public function roles()
+    	{
+    		return $this->belongsToMany('App\Role')->withTimestamps();
+    	}
+
+	public function authorizeRoles($roles)
+	{
+	    if ($this->hasAnyRole($roles)) {
+	        return true;
+	    }
+	    abort(401, 'Esta acción no está autorizada.');
+
+	    //return response()->json(['message' => 'Unauthorized'], 401);
+	}
+
+	public function hasAnyRole($roles)
+	{
+	    if (is_array($roles)) {
+		        foreach ($roles as $role) {
+		            if ($this->hasRole($role)) {
+		                return true;
+		            }
+	        }
+	    } else {
+		        if ($this->hasRole($roles)) {
+			            return true;
+        	}
+    	}
+    	return false;
+	}
+
+	public function hasRole($role)
+	{
+	    if ($this->roles()->where('name', $role)->first()) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	public function purchaseOrders()
+    {
+        return $this->hasMany('App\PurchaseOrder');
+    }
 }
